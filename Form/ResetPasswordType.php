a025618c8826ba6a0046de1b2c5339e4c5292022
Damien Duboeuf <smeagolworms4@gmail.com>
2016-09-09 14:26:32 +0200

    Fix Form Login

---
+++ Form/ResetPasswordType.php
@@ -0,0 +1,13 @@
+<?php
+namespace GollumSF\AuthRestBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * RegisterType
+ *
+ * @author Damien Duboeuf <smeagolworms4@gmail.com>
+ */
+class RegisterType extends AbstractType{
+	
+}
